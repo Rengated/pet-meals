884ffb70507946965951515614e14569225f6b05
rengated <rengated@MacBook-Air-rengated.local>
2022-07-31 15:46:48 +0300

    added

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from '../styles/footer.module.css'
+
+export const Footer = () => {
+
+   return(
+    <div className={styles.footer}>
+        <p className={styles.name}>Rengated, 2022 @</p>
+    </div>
+   )
+
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,13 @@
 import { Navbar } from "./Navbar"
- 
+import { Footer } from "./Footer"
+
  export const Layot = ({children}) => {
     return(
-        <>
-            <div className="container"
-                ><Navbar/>
-            </div>
-
-            <div className="layout">
-                <div className=""> {children} </div>
-            </div>      
-        </>
+        <div>
+            <div className="container"><Navbar/></div>
+            <div className="layout">{children}</div>
+            <div className="container"><Footer/></div>
+         </div>
     )
 
 
